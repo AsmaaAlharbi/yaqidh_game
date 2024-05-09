bf9865348e6dc73c16604bbae0a6ce0bdaeeefe2
AsmaaAlharbi <i.sooomx@gmail.com>
2024-05-09 15:05:11 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:yaqidh_game/analytics/camera.dart';
 import 'package:yaqidh_game/screens/menu_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
   Camera.initCamera();
 }
