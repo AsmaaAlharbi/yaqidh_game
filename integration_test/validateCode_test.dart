20b77ae4017dc4d8118679d9ff7b4faacf6b060c
AsmaaAlharbi <i.sooomx@gmail.com>
2024-05-10 21:47:26 +0300

    .

---
+++ integration_test/validateCode_test.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:yaqidh_game/screens/task_screen.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:yaqidh_game/widgets/image_button.dart';
+import 'package:yaqidh_game/main.dart' as app;
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('end-to-end test', () {
+    testWidgets(
+      'verify error dialog for valid but already tested code',
+      (tester) async {
+        print('Starting test for valid but already tested code...');
+        app.main();
+        await tester.pumpAndSettle();
+
+        // Ensure the TextField is available
+        expect(find.byType(TextField), findsWidgets);
+
+        await tester.enterText(find.byType(TextField).first, '228680');
+        await Future.delayed(const Duration(seconds: 3));
+        await tester.pumpAndSettle();
+        await tester.tap(find.byType(ImageButton).first);
+        await Future.delayed(const Duration(seconds: 3));
+        await tester.pumpAndSettle();
+
+        String studentName = 'رهف صالح'; // Replace with actual expected student name
+        print('Checking for error dialog with student name...');
+        expect(find.textContaining(' ($studentName)  لقد تم اختبار الطالب'),
+            findsOneWidget);
+        await Future.delayed(const Duration(seconds: 5));
+        print('Test for valid but already tested code passed.');
+      },
+    );
+
+    testWidgets(
+      'verify error dialog for invalid code',
+      (tester) async {
+        print('Starting test for invalid code...');
+        app.main();
+        await tester.pumpAndSettle();
+
+        // Ensure the TextField is available
+        expect(find.byType(TextField), findsWidgets);
+
+        await tester.enterText(find.byType(TextField).first, '123456');
+        await Future.delayed(const Duration(seconds: 3));
+        await tester.pumpAndSettle();
+        await tester.tap(find.byType(ImageButton).first);
+        await Future.delayed(const Duration(seconds: 3));
+        await tester.pumpAndSettle();
+
+        print('Checking for error dialog with invalid code message...');
+        expect(find.text('الكود غير صالح '), findsOneWidget);
+        await Future.delayed(const Duration(seconds: 5));
+        print('Test for invalid code passed.');
+      },
+    );
+
+    testWidgets(
+      'verify main screen with valid and not tested code',
+      (tester) async {
+        print('Starting test for valid code and not tested...');
+        app.main();
+        await tester.pumpAndSettle();
+
+        // Ensure the TextField is available
+        expect(find.byType(TextField), findsWidgets);
+
+        await tester.enterText(find.byType(TextField).first, '625085');
+        await Future.delayed(const Duration(seconds: 3));
+        await tester.pumpAndSettle();
+        await tester.tap(find.byType(ImageButton).first);
+        await Future.delayed(const Duration(seconds: 3));
+        await tester.pumpAndSettle();
+
+        print('Checking for TaskScreen...');
+        expect(
+          find.byWidgetPredicate(
+            (Widget widget) =>
+                widget is TaskScreen &&
+                widget.levelId == 0 &&
+                widget.studentId == '22536159',
+          ),
+          findsOneWidget,
+        );
+        await Future.delayed(const Duration(seconds: 3));
+        print('Test for valid code and not tested passed.');
+      },
+    );
+  });
+}
